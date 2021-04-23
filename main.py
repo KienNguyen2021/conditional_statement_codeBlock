7f135f2feae004042b9171ff6b18761d044b3f88
kien1nguyen <>
2021-04-23 02:37:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print ("Welcome to the rollercoaster ! ")
+
+height = int (input(" what is your height in cm ? "))
+
+if height == 20 :
+  print (" You can ride the rollercoaster")
+else :
+  print (" Sorry ! you have to grow taller before you can ride ")  
+
+number = int (input(" Enter a number : "))  
+
+if number % 2 == 0 :
+  print(f" Your number : {number} is Even number")
+else :
+  print(f" Your number {number} is Odd number ")  
